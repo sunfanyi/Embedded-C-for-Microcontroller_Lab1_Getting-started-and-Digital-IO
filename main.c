68d8dbb4d6c24379647062e18cefb1d3bc5fcab7
sunfanyi <sunfanyi_jim@outlook.com>
2021-10-12 10:44:33 +0100

    Make LED 2 (pin RH3) come on when LED 1 (RD7) is not

--- main.c
+++ main.c
@@ -20,20 +20,27 @@
  * ...
 ************************************/
 void main(void) {    
-    // setup pin for output (connected to LED)
-    LATDbits.LATD7=0;   //set initial output state
-    TRISDbits.TRISD7=0; //set TRIS value for pin (output)
+    // setup pin for output (connected to LED) RD7:LED1
+    LATDbits.LATD7=0;   //set initial output state for LED1
+    TRISDbits.TRISD7=0; //set TRIS value for pin 1 (output) TRIS 0 means output
+    LATHbits.LATH3=0;   //set initial output state for LED2
+    TRISHbits.TRISH3=0; //set TRIS value for pin 2 (output)
     
-    // setup pin for input (connected to button)
-    TRISFbits.TRISF2=1; //set TRIS value for pin (input)
-    ANSELFbits.ANSELF2=0; //turn off analogue input on pin  
+    // setup pin for input (connected to button) RF2: button1
+    TRISFbits.TRISF2=1; //set TRIS value for pin 1 (input) TRIS 1 means input
+    ANSELFbits.ANSELF2=0; //turn off analogue input on pin 1
+    TRISFbits.TRISF3=1; //set TRIS value for pin 2 (input)
+    ANSELFbits.ANSELF3=0; //turn off analogue input on pin 2
     
     while (1) { //infinite while loop - repeat forever
         
         while (PORTFbits.RF2); //empty while loop (wait for button press)
         
-        if (!PORTFbits.RF2) LATDbits.LATD7 = !LATDbits.LATD7; //toggle LED
-
+        if (!PORTFbits.RF2) {
+            LATDbits.LATD7 = !LATDbits.LATD7; //toggle LED 1
+            LATHbits.LATH3 = !LATDbits.LATD7; //LED 2 come on when LED 1 is not
+        }
+        
         __delay_ms(200); // call built in delay function 
     }
 }
